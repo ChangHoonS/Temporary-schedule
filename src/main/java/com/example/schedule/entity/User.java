da96fee890074b2c61793b3f6c7c26d775761f9a
ChnagHoonS <bba01219@gmail.com>
2025-04-03 10:16:57 +0900

    feat: 레벨2 유저 생성 기능(비밀번호 없는 회원가입) 만들기 완료

--- src/main/java/com/example/schedule/entity/User.java
+++ src/main/java/com/example/schedule/entity/User.java
@@ -19,4 +19,13 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String email;
 
+    // Entity는 기본 생성자가 필요하기 때문에 만들어줌 (새로운 생성자 만들면 기본 생성자가 사라지기 때문)
+    public User() {
+    }
+
+    // 회원가입에 필요한 생성자를 만들어줌 (Lv2 단계에서는 비밀번호를 넣지 않음)
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/example/schedule/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.schedule.service;
+
+import com.example.schedule.dto.SignUpResponseDto;
+import com.example.schedule.entity.User;
+import com.example.schedule.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public SignUpResponseDto signUp(String username, String email) {
+
+        User user = new User(username, email);
+
+        User savedUser = userRepository.save(user);
+
+        return new SignUpResponseDto(savedUser.getId(), savedUser.getUsername(), savedUser.getEmail());
+    }
+}
